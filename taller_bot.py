f1f7be2fd21b4df2d286935e3cf8c532d1056f05
Dmitry <vdl99@list.ru>
2023-08-02 12:36:44 +0300

    перед формированием алгоритма загрузки данных с диска и API

--- taller_bot.py
+++ taller_bot.py
@@ -7,22 +7,24 @@ Tallers_bot
 
 t.me/Tallers_bot
 """
+from tb_sec_set import TB_TOKEN
+from tb_settings import *
 from datetime import datetime
-import tb_settings
 import tb_dict_currency
 import telebot
 import requests
 import json
-from extensions import APIexceptions
-import lxml
-import lxml.html
-from lxml import etree
-import urllib3
+import pickle
+# from extensions import APIexceptions
+# import lxml
+# import lxml.html
+# from lxml import etree
+# import urllib3
 
 import currencyapicom
-client = currencyapicom.Client(tb_settings.CR_TOKEN)
+client = currencyapicom.Client(CR_TOKEN)
 
-bot = telebot.TeleBot(tb_settings.TB_TOKEN)
+bot = telebot.TeleBot(TB_TOKEN)
 
 
 def date_time_stamp(date_message=None):
@@ -48,8 +50,9 @@ def day_time_sender(_message):
 
 
 def get_all_currency():
+    r = None
     try:
-        r = requests.get(tb_settings.CR_REQUEST_CR_LATE)  # запрашиваем текущие курсы всех валют
+        r = requests.get(CR_REQUEST_CR_LATE)  # запрашиваем текущие курсы всех валют
         _curr = json.loads(r.content)  # делаем из полученных байтов Python-объект для удобной работы
     except requests.exceptions.ReadTimeout:
         _curr = None
@@ -62,8 +65,9 @@ def get_all_currency():
 
 
 def get_all_currency_info():
+    r = None
     try:
-        r = requests.get(tb_settings.CR_REQUEST_CR_LIST)  # запрашиваем текущие курсы всех валют
+        r = requests.get(CR_REQUEST_CR_LIST)  # запрашиваем текущие курсы всех валют
         _curr = json.loads(r.content)  # делаем из полученных байтов Python-объект для удобной работы
     except requests.exceptions.ReadTimeout:
         _curr = None
@@ -77,7 +81,7 @@ def get_all_currency_info():
 
 def get_currency_pair(val1, val2, amo):
     try:
-        r = requests.get(tb_settings.CR_REQUEST_CR_LIST + val1 + "%2C" + val2)  # запрашиваем текущие курсы двух валют
+        r = requests.get(CR_REQUEST_CR_LIST + val1 + "%2C" + val2)  # запрашиваем текущие курсы двух валют
         _curr = json.loads(r.content)  # делаем из полученных байтов Python-объект для удобной работы
     except requests.exceptions.ReadTimeout:
         _curr = None
@@ -97,31 +101,15 @@ def all_currency_list_out(_message):
         if curr_list['data'][keys] is not None:
             s = ''
             if curr_info['data'][keys] is not None:
-                try: # дополнительная информация о валюте есть
+                try:  # дополнительная информация о валюте есть
                     s = curr_info['data'][keys]['name_plural'] + '\n'
-                except TypeError: # ошибка в информационном описании валюты
+                except TypeError:  # ошибка в информационном описании валюты
                     print('For key = [' + keys + ']')  # беду в консоль
                     print(curr_info['data'][keys])
                 else:  # дополнительной информации о валюте нет
                     pass
-            # ========================== блок форматирования вывода по правилам округления из API
             v_to_round = curr_list['data'][keys]['value']
-            #
-            # dec_dig = None  # нужна при выводе в консоль
-            # if v_to_round < 0.0001:  # для малых значений ставим 10 знаков после запятой
-            #     s_rounded = '{0:.10f}'.format(v_to_round)
-            # elif len(s) != 0:  # нашли доп инфо по валюте и берем из нее число знаков после запятой
-            #     dec_dig = curr_info['data'][keys]['decimal_digits']
-            #     s_rounded = '%.' + str(dec_dig) + 'f'
-            #     s_rounded = s_rounded % v_to_round
-            # else:  # доп инфо по валюте нет - выставляем обычный тип формата
-            #     s_rounded = '{0:f}'.format(v_to_round)
-            # if float(s_rounded) == 0:  # проверяем на потерю точности и правим ее по обнаружении
-            #     s_rounded = '*' + '{0:f}'.format(v_to_round)
-            
             s_rounded = fmt_rnd(v_to_round, keys)
-            
-            # ==========================
             s += curr_list['data'][keys]['code'] + '\t = ' + s_rounded
             # print(f'Formatted [{keys}/{dec_dig}] =', s_rounded, '\t\t\t\t\t', v_to_round)  # в консоль не выводим
             texts += s + '\n'
@@ -154,6 +142,7 @@ def fmt_rnd(_v_to_round, _keys):
         _s_rounded = '*' + '{0:f}'.format(_v_to_round)
     return _s_rounded
 
+
 """
 @bot.message_handler(filters)
 def function_name(message):
@@ -167,7 +156,7 @@ def handle_start_help(message):
     print(day_time_sender(message))
     name = str(message.chat.first_name)
     bot.send_message(message.chat.id, 'Привет тебе, ' + name + ', забредший сюда.')
-    bot.send_message(message.chat.id, tb_settings.H_TEXT)
+    bot.send_message(message.chat.id, H_TEXT)
     
     
 # Обрабатывается запрос конвертации валют
@@ -191,11 +180,11 @@ def handle_exchange(message):
             if val1 == val2:
                 raise KeyError
         except KeyError:
-            bot.send_message(message.chat.id, str('Код валюты введен с ошибкой!\n\n' + tb_settings.H_TEXT))
+            bot.send_message(message.chat.id, str('Код валюты введен с ошибкой!\n\n' + H_TEXT))
             print('Код валюты введен с ошибкой =', cmd_ln)
             return
         except ValueError:
-            bot.send_message(message.chat.id, str('Число введено с ошибкой!\n\n' + tb_settings.H_TEXT))
+            bot.send_message(message.chat.id, str('Число введено с ошибкой!\n\n' + H_TEXT))
             print('Число введено с ошибкой =', cmd_ln)
             return
         # наличие подтверждено
@@ -211,8 +200,6 @@ def handle_exchange(message):
         print(val2, ddg2, fmt_rnd(rate2, val2), rate2)
         print(val1, ddg1, fmt_rnd(result, val1), result)
         
-        
-        
         dec_v1 = '%.' + str(curr_info['data'][val1]['decimal_digits']) + 'f'
         dec_v1 = dec_v1 % amount
         dec_v2 = '%.' + str(curr_info['data'][val2]['decimal_digits']) + 'f'
@@ -222,7 +209,7 @@ def handle_exchange(message):
         bot.send_message(message.chat.id, s)
     else:
         bot.send_message(message.chat.id, str('Что Вы имели ввиду набрав:\n"' +
-                                              str(cmd_ln) + '"?\n\n' + tb_settings.H_TEXT))
+                                              str(cmd_ln) + '"?\n\n' + H_TEXT))
     
 
 # Обрабатывается загрузка списка валют в словарь из запроса по API к сервису
@@ -238,8 +225,31 @@ def handle_load_values(message):
         curr_dummy = get_all_currency_info()
         if curr_dummy is not None:
             tb_dict_currency.currencies_info = curr_dummy
+            curr_info = curr_dummy  # все считалось
+            with open('tlbdatacl.pkl', 'wb') as f:  # сохраним в файл
+                pickle.dump(tb_dict_currency.currencies_list, f)
+            with open('tlbdatain.pkl', 'wb') as f:  # сохраним в файл
+                pickle.dump(tb_dict_currency.currencies_info, f)
+            return
+    print(date_time_stamp(), 'Нет связи с БД валют через API!')
+    return
+
+
+# Обрабатывается записи словаря из запроса по API к сервису на диск
+@bot.message_handler(commands=['w'])
+def handle_load_values(message):
+    global curr_list, curr_info
+    print(day_time_sender(message))
+    print(date_time_stamp(), 'запрос данных через API')
+    curr_dummy = get_all_currency()
+    if curr_dummy is not None:
+        tb_dict_currency.currencies_list = curr_dummy
+        curr_list = curr_dummy
+        curr_dummy = get_all_currency_info()
+        if curr_dummy is not None:
+            tb_dict_currency.currencies_info = curr_dummy
             curr_info = curr_dummy
-            return # все считалось
+            return  # все считалось
     print(date_time_stamp(), 'Нет связи с БД валют через API!')
     return
 
@@ -259,167 +269,88 @@ def other_messages(message):
     bot.send_message(message.chat.id, str('Что Вы имели ввиду набрав:\n"' +
                                           message.text +
                                           '"? В помощь:\n' +
-                                          tb_settings.H_TEXT))
+                                          H_TEXT))
+
+
+def reorder_dt(rs):
+    s = rs[8:10] + '-' + rs[5:7] + '-' + rs[0:4] + ' ' + rs[11:19]
+    return s
+
+
+def load_dict(fn, dict_struct):
+    try:
+        with open(fn, 'rb') as f:
+            dict_struct = pickle.load(f)
+            print('Загружена база', fn, '\n', dict_struct)
+            return True
+    except FileNotFoundError:
+        print('Файл ', fn, 'загрузить с диска не удалось. Используется старый.')
+    except UnicodeDecodeError:
+        print('Файл ', fn, 'содержит ошибки. Используется старый.')
+    except pickle.UnpicklingError:
+        print('Файл ', fn, 'содержит ошибки. Используется старый.')
+    return False
+
+
+def save_dict(fn, dict_struct):
+    try:
+        with open(fn, 'wb') as f:
+            pickle.dump(dict_struct, f)
+            print('Записан файл', fn, '\n', dict_struct)
+            return True
+    except FileNotFoundError:
+        print('Файл ', fn, 'записать на диск не удалось.')
+    except UnicodeDecodeError:
+        print('Файл ', fn, 'содержит ошибки.')
+    except pickle.UnpicklingError:
+        print('Файл ', fn, 'содержит ошибки.')
+    return False
+
+
+def not_up_to_date():
+    last_update = curr_list['meta']['last_updated_at']
+    print(reorder_dt(last_update), 'Загруженная версия котировок.')
+    ts1 = datetime(int(last_update[0:4]),
+                   int(last_update[5:7]),
+                   int(last_update[8:10]),
+                   int(last_update[11:13]),
+                   int(last_update[14:16]),
+                   int(last_update[17:19])) \
+        .timestamp()
+    ts2 = datetime.now().timestamp()
+    return (ts2-ts1) > UPD_INTERVAL_SEC
+
+
+def base_load():
+    global curr_list, curr_info
+    error_flag = 0
+    if not_up_to_date():
+        print('Устаревшие данные, загружаем из файлов.')
+        if not load_dict('tlbdatacl.pkl', tb_dict_currency.currencies_list):
+            error_flag += 1
+        if not load_dict('tlbdatain.pkl', tb_dict_currency.currencies_info):
+            error_flag += 2
+    
+    if error_flag & 1 != 1:
+        curr_list = tb_dict_currency.currencies_list
+        
+    if error_flag & 2 != 2:
+        curr_info = tb_dict_currency.currencies_info
+    
+    last_update = reorder_dt(tb_dict_currency.currencies_list['meta']['last_updated_at'])
+    print(last_update, 'используемая версия котировок.')
+    return error_flag
 
 
 """
 main +++++++++++++++++++++++++++++++++++++++++++
 """
-
 print(date_time_stamp(), "Телеграмм бот по обмену валют запущен")
-
-
+# загрузка словарей валюты и правил округления (по умолчанию - старые данные)
 curr_list = tb_dict_currency.currencies_list
 curr_info = tb_dict_currency.currencies_info
-bot.infinity_polling()
-# while True:
-#     try:
-#
-#     except urllib3.exceptions.ReadTimeoutError:
-#         break
-#     except requests.exceptions.ReadTimeout:
-#         break
-print(date_time_stamp(), 'Что-то пошло не так!')
+base_load()
 
-#
-#
+#bot.infinity_polling()
 
-"""
-'id': 864491376, 'first_name': 'Natasha'
-'id': 463882236, 'first_name': '.V.aSko.V.'
-'id': 275588773, 'first_name': 'Dmitry'
-'id': 224489387, 'first_name': 'Gidro', 'username': 'Gidro_Gidro', 'last_name': 'Gidro'
-
-{
-'content_type': 'text',
-'id': 48,
-'message_id': 48,
-'from_user': {
-                'id': 463882236,
-                'is_bot': False,
-                'first_name': '.V.aSko.V.',
-                'username': 'kitty1ket',
-                'last_name': None,
-                'language_code': 'ru',
-                'can_join_groups': None,
-                'can_read_all_group_messages': None,
-                'supports_inline_queries': None,
-                'is_premium': None,
-                'added_to_attachment_menu': None
-             },
-'date': 1689316154,
-'chat': {
-            'id': 463882236,
-            'type': 'private',
-            'title': None,
-            'username': 'kitty1ket',
-            'first_name': '.V.aSko.V.',
-            'last_name': None,
-            'is_forum': None,
-            'photo': None,
-            'bio': None,
-            'join_to_send_messages': None,
-            'join_by_request': None,
-            'has_private_forwards': None,
-            'has_restricted_voice_and_video_messages': None,
-            'description': None,
-            'invite_link': None,
-            'pinned_message': None,
-            'permissions': None,
-            'slow_mode_delay': None,
-            'message_auto_delete_time': None,
-            'has_protected_content': None,
-            'sticker_set_name': None,
-            'can_set_sticker_set': None,
-            'linked_chat_id': None,
-            'location': None,
-            'active_usernames': None,
-            'emoji_status_custom_emoji_id': None,
-            'has_hidden_members': None,
-            'has_aggressive_anti_spam_enabled': None
-        },
-'sender_chat': None,
-'forward_from': None,
-'forward_from_chat': None,
-'forward_from_message_id': None,
-'forward_signature': None,
-'forward_sender_name': None,
-'forward_date': None,
-'is_automatic_forward': None,
-'reply_to_message': None,
-'via_bot': None,
-'edit_date': None,
-'has_protected_content': None,
-'media_group_id': None,
-'author_signature': None,
-'text': '/start',
-'entities': [<telebot.types.MessageEntity object at 0x0338CE38>],
-'caption_entities': None,
-'audio': None,
-'document': None,
-'photo': None,
-'sticker': None,
-'video': None,
-'video_note': None,
-'voice': None,
-'caption': None,
-'contact': None,
-'location': None,
-'venue': None,
-'animation': None,
-'dice': None,
-'new_chat_member': None,
-'new_chat_members': None,
-'left_chat_member': None,
-'new_chat_title': None,
-'new_chat_photo': None,
-'delete_chat_photo': None,
-'group_chat_created': None,
-'supergroup_chat_created': None,
-'channel_chat_created': None,
-'migrate_to_chat_id': None,
-'migrate_from_chat_id': None,
-'pinned_message': None,
-'invoice': None,
-'successful_payment': None,
-'connected_website': None,
-'reply_markup': None,
-'message_thread_id': None,
-'is_topic_message': None,
-'forum_topic_created': None,
-'forum_topic_closed': None,
-'forum_topic_reopened': None,
-'has_media_spoiler': None,
-'forum_topic_edited': None,
-'general_forum_topic_hidden': None,
-'general_forum_topic_unhidden': None,
-'write_access_allowed': None,
-'user_shared': None,
-'chat_shared': None,
-'json':
-        {
-            'message_id': 48,
-            'from':
-                    {
-                        'id': 463882236,
-                        'is_bot': False,
-                        'first_name': '.V.aSko.V.',
-                        'username': 'kitty1ket',
-                        'language_code': 'ru'
-                    },
-            'chat': {
-                        'id': 463882236,
-                        'first_name': '.V.aSko.V.',
-                        'username': 'kitty1ket',
-                        'type': 'private'},
-            'date': 1689316154,
-            'text': '/start',
-            'entities': [{
-                            'offset': 0,
-                            'length': 6,
-                            'type': 'bot_command'
-                         }
-                        ]
-            }
-        }
-"""
+#print(date_time_stamp(), '\n\nЧто-то пошло не так!')
